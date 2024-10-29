1fc3c7f55958d86868d3307d5f942364a6390b10
RBLeon <raynleon99@gmail.com>
2024-10-29 19:56:52 +0100

    Inital working android push notification + analytics

--- MauiPluginFirebaseMVP/MauiProgram.cs
+++ MauiPluginFirebaseMVP/MauiProgram.cs
@@ -1,4 +1,14 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
+using Plugin.Firebase.Auth;
+using Plugin.Firebase.Bundled.Shared;
+using Plugin.Firebase.Crashlytics;
+
+#if ANDROID
+using Plugin.Firebase.Bundled.Platforms.Android;
+#elif IOS
+using Plugin.Firebase.Bundled.Platforms.iOS;
+#endif
 
 namespace MauiPluginFirebaseMVP;
 
@@ -9,6 +19,7 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
+			.RegisterFirebaseServices()
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
@@ -22,5 +33,34 @@ public static class MauiProgram
 #endif
 
 		return builder.Build();
+	}
+	
+	private static MauiAppBuilder RegisterFirebaseServices(this MauiAppBuilder builder)
+	{
+		builder.ConfigureLifecycleEvents(events => {
+#if IOS
+            events.AddiOS(iOS => iOS.FinishedLaunching((app, launchOptions) => {
+                CrossFirebase.Initialize(CreateCrossFirebaseSettings());
+                CrossFirebaseCrashlytics.Current.SetCrashlyticsCollectionEnabled(true);
+                return false;
+            }));
+#else
+			events.AddAndroid(android => android.OnCreate((activity, _) =>
+				CrossFirebase.Initialize(activity, CreateCrossFirebaseSettings())));
+			CrossFirebaseCrashlytics.Current.SetCrashlyticsCollectionEnabled(true);
+#endif
+		});
+
+		builder.Services.AddSingleton(_ => CrossFirebaseAuth.Current);
+		return builder;
+	}
+
+	private static CrossFirebaseSettings CreateCrossFirebaseSettings()
+	{
+		return new CrossFirebaseSettings(
+			isAnalyticsEnabled: true,
+			isAuthEnabled: true,
+			isCloudMessagingEnabled: true,
+			isCrashlyticsEnabled: true);
 	}
 }
